bfb8fce0e63705be151de673bc5bd02380469638
SurajGiri55446 <surajgiri@55446gmail.com>
2025-12-25 22:03:42 +0530

    eveything works together

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -232,3 +232,26 @@ export const getfollowingpost = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+export const getuserpost = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    // ❗ use findOne, not find
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate("user", "-password");
+
+    res.status(200).json(posts);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { protectroute } from "../middleware/protectroute.middleware.js";
-import { commentpost, createPost, deletepost, getAllPostsByUser, getfollowingpost, likedbyuser, likePost } from "../controllers/post.controller.js";
+import { commentpost, createPost, deletepost, getAllPostsByUser, getfollowingpost, getuserpost, likedbyuser, likePost } from "../controllers/post.controller.js";
 
 const router=express.Router();
 
 router.post("/create-post",protectroute,createPost)
 router.delete("/delete-post/:id",protectroute,deletepost)
 router.post("/like-post/:id",protectroute,likePost)
+router.get("/get-user-post/:username",protectroute,getuserpost)
 router.get("/liked-post/:id",protectroute,likedbyuser)
 router.post("/comment-post/:postid",protectroute,commentpost)
 router.get("/get-all-post/:id",protectroute,getAllPostsByUser)
